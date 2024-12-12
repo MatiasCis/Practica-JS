044e679401d9b523d86a83c24cdfdbf09a1f3dd0
Matias <mgcm03@hotmail.com>
2024-12-12 16:58:39 -0300

    BlackJack-Vite

---
+++ blackjack-vite/src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,47 @@
+import { pedirCarta, valorCarta, crearCartaHtml } from "./";
+
+
+// turno de la computadora
+/**
+ * 
+ * @param {Number} puntosMinimos puntos minimos que la computadora necesita para ganar
+ * @param {HTMLElement} puntosHTML
+ * @param {HTMLElement} divCartasComputadora Elemento HTML para mostrar los puntos 
+ * @param {Array<String>} deck 
+ */
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = []) => {
+
+    if ( !puntosMinimos ) throw new Error('Puntos minimos son necesarios');
+    if ( !puntosHTML ) throw new Error(' Argumento untos HTML es necesario');
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta(deck);
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        // <img class="carta" src="assets/cartas/2C.png">
+        const imgCarta = crearCartaHtml( carta );
+        divCartasComputadora.append( imgCarta );
+
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
